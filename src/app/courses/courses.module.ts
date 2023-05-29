b057062dfd9fea9c6e81431f89b524c632309a6c
Erika Santos <dossantos.erika08@gmail.com>
2023-05-29 17:24:42 -0300

    Add Material table and Card

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './courses/courses.component';
 
+import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,9 @@ import { CoursesComponent } from './courses/courses.component';
   ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    AppMaterialModule
+   
   ]
 })
 export class CoursesModule { }
