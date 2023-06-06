911d39b59780abf12820a0a8f2fe5f57beea17e7
Erika Santos <dossantos.erika08@gmail.com>
2023-06-06 17:58:21 -0300

    Add spinner

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -9,12 +9,12 @@ import { Observable } from 'rxjs';
 })
 export class CoursesComponent  {
 
-  courses: Observable<Course []>;
+  courses$: Observable<Course []>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService)  {
     //this.courses = [];
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
 
   }
 
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { first, tap, delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,8 @@ export class CoursesService {
    
       return this.httpClient.get<Course[]>(this.API)
       .pipe(
+        first(),
+        delay(15000),
         tap(courses => console.log(courses))
       );   
   }
