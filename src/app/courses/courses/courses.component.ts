870d164a0f7973c27659811acf6ebfa093f50b4a
Erika Santos <dossantos.erika08@gmail.com>
2023-06-09 16:38:23 -0300

    Add error dialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -12,10 +14,24 @@ export class CoursesComponent  {
   courses$: Observable<Course []>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService)  {
+  constructor(
+    public dialog: MatDialog,
+    private coursesService: CoursesService)  {
     //this.courses = [];
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar curso')
+        return of([])
+      })
+      
+    );
+  }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, { 
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
