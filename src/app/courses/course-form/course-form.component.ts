f1129999fdcce4953e436648b2c25e5eca062af6
Erika Santos <dossantos.erika08@gmail.com>
2023-07-03 17:13:17 -0300

    Save datas with http post

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -8,21 +12,38 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 
 
-export class CourseFormComponent  {
+export class CourseFormComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private coursesService: CoursesService,
+    private _snackBar: MatSnackBar
+    ) {
+
+      //Leva o parametro a um objeto correspondente ao
     this.form = this.formBuilder.group({
       name: [null],
-      category:[null]
+      category: [null],
     });
-     }
+  }
 
-     ngOnInit(): void {
-}
+     ngOnInit(): void {}
 
   onSubmit() {
+    this.coursesService.saveCourse(this.form.value).subscribe({  
+      next: (data) => console.log(data),
+      error: () => {
+        this.onError();
+   },
+});
 
+  }
+
+  private onError(): void {
+    this._snackBar.open('Erro ao salvar o curso!', '', {
+      duration: 3000,
+    });
   }
 
   onCancel() {
